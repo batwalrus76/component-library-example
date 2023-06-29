63a36fe6a23afaf51b4bcf5c457a6f4e9944ce5f
Chancellor Pascale <chancebatwalrus@gmail.com>
2023-06-29 14:39:46 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TestComponent } from "harvey-component-library";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TestComponent theme="secondary" />
+    </div>
+  );
+}
